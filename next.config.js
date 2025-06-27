ead2a6d848425cadff0ef8435dfff7a078cd0479
CanliDestek <info@canlidestek.com>
2025-06-27 22:31:47 +0300

    MIME tipi sorunu düzeltildi

--- next.config.js
+++ next.config.js
@@ -2,8 +2,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: false,
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/html; charset=UTF-8",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
